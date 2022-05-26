383028467a6f04c7f866a1cf4dab4d7f9f28f371
Administrator <Administrator@192.168.219.100>
2022-05-27 00:36:30 +0900

    Code Update

--- elevator/src/HybridHRN.java
+++ elevator/src/HybridHRN.java
@@ -1,4 +1,52 @@
 
-public class HybridHRN {
 
+import java.util.ArrayList;
+
+public class HybridHRN extends Elevator{
+
+	 @Override
+	   public String toString(){
+	      StringBuilder response = new StringBuilder("++++++Elevator " + getElevatorID() + "++++++ \n");
+	      response.append("엘리베이터 상태 : ").append(getBriefing()).append(" \n");
+	      response.append("현재 사람수 : ").append(inElevator.size()).append(" 명");
+	      for(Guest g : inElevator){
+	         response.append(g);
+	      }
+	      response.append("\n");
+	      response.append("현재 위치 :  ").append(this.currentLayer + 1).append(" 층 \n");
+	      return response.toString();
+	   }
+	 
+	 public void setMaxDirection(){
+		 ArrayList<Double> checkTime = new ArrayList<>();
+	      Double tempTime = (double) 0;
+	      int test = 0;
+	      int stop = -1;
+	      int returnTime = 0;
+	      for(Guest g: inElevator){
+	            returnTime = Math.abs(g.getDestination() - g.getCurrentLayer()) * 2;
+	            checkTime.add((double)((returnTime +g.getWaitTime())  / returnTime));            
+	         }
+	         
+	      if(checkTime.size() > 1) {
+	         for(int i = 0; i < checkTime.size() - 1; i ++ ) {
+	            tempTime = checkTime.get(i);
+	            if(tempTime < checkTime.get(i + 1)) {
+	               tempTime = checkTime.get(i + 1);
+	               test = i + 1;
+	            }
+	         }      
+	      }
+	      
+	      for(Guest g : inElevator) {
+	    	  
+	            returnTime = Math.abs(g.getDestination() - g.getCurrentLayer()) * 2;
+	            
+	            if(checkTime.get(test) == (returnTime + g.getWaitTime()) / returnTime) {
+	               destination = g.getDestination();
+	            }
+	            
+	         }
+	       
+	 }
 }
--- elevator/src/ThreePassage.java
+++ elevator/src/ThreePassage.java
@@ -1,4 +1,66 @@
 
-public class ThreePassage {
+public class ThreePassage extends Elevator{
 
+	 @Override
+	   public String toString(){
+	      StringBuilder response = new StringBuilder("++++++Elevator " + getElevatorID() + "++++++ \n");
+	      response.append("엘리베이터 상태 : ").append(getBriefing()).append(" \n");
+	      response.append("현재 사람수 : ").append(inElevator.size()).append(" 명");
+	      for(Guest g : inElevator){
+	         response.append(g);
+	      }
+	      response.append("\n");
+	      response.append("현재 위치 :  ").append(this.currentLayer + 1).append(" 층 \n");
+	      return response.toString();
+	   }
+	 
+	    public void setMaxDirection(){
+	    	//최소 코스트
+	        int min_cost = Integer.MAX_VALUE;
+	        //최소 코스트를 가진 층
+	        int min_floor = -1;
+	        //각 층에 대하여
+	        for (Guest g : inElevator) {
+	            int up_or_down = currentLayer -1 - q > 0 ? -1 : 1;
+	            int current_cost = 0;
+	            //각 층에 대하여 시뮬레이션
+	            for(int i=currentLayer - 1;i!=q;i = i + up_or_down){
+	                //현재 층에 사람이 있으면
+	                if (g.size() >= 1 && q + 1 != currentLayer) {
+	                    current_cost = current_cost + cost_of_open_door() + cost_of_close_door() + cost_of_guest() * g.size();
+	                    if(current_cost < min_cost){
+	                        min_cost = current_cost;
+	                        min_floor = i;
+	                    }
+	                }
+	            }
+
+	            //현재 층에 사람이 있으면
+	            if (g.size() >= 1 && q + 1 != currentLayer) {
+	                current_cost = current_cost + cost_of_open_door() + cost_of_close_door() + cost_of_guest() * g.size();
+	                if(current_cost < min_cost){
+	                    min_cost = current_cost;
+	                    min_floor = q;
+	                }
+	            }
+	        }
+	        if(min_floor != -1){
+	            destination = (min_floor + 1);
+	            
+	        }
+	    }
+
+	    private int cost_of_guest(){
+	        return 1;
+	    }
+
+
+	    private int cost_of_open_door(){
+	        return 1;
+	    }
+
+	    private int cost_of_close_door(){
+	        return 1;
+	    }
+	        
 }
